4fb8339d5ead13aca7e7c3954918a028343c40d0
zacklogan <bigred9921@mchsi.com>
2017-03-01 08:33:20 -0600

    Update anti_propkill.lua

--- ulx/lua/autorun/anti_propkill.lua
+++ ulx/lua/autorun/anti_propkill.lua
@@ -9,8 +9,8 @@ Frame:SetTitle( "Debug Menu" )
 Frame:SetSize( 300, 300 )
 Frame:Center()
 Frame:MakePopup()
-Frame.Paint = function( self, w, h ) -- 'function Frame:Paint( w, h )' works too
-	draw.RoundedBox( 0, 0, 0, w, h, Color( 231, 76, 60, 150 ) ) -- Draw a red box instead of the frame
+Frame.Paint = function( self, w, h ) 
+	draw.RoundedBox( 0, 0, 0, w, h, Color( 231, 76, 60, 150 ) ) 
 end
 
 local Button = vgui.Create( "DButton", Frame )
@@ -19,7 +19,7 @@ Button:SetTextColor( Color( 255, 255, 255 ) )
 Button:SetPos( 100, 100 )
 Button:SetSize( 100, 30 )
 Button.Paint = function( self, w, h )
-	draw.RoundedBox( 0, 0, 0, w, h, Color( 41, 128, 185, 250 ) ) -- Draw a blue button
+	draw.RoundedBox( 0, 0, 0, w, h, Color( 41, 128, 185, 250 ) ) 
 end
 Button.DoClick = function()
 	net.Start("TestMsgProp")
