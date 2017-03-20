1ab5f216b78bb2c7667948d29aede77e8756c33d
zacklogan <bigred9921@mchsi.com>
2017-03-20 14:18:29 -0500

    Update maintenance_mode.lua

--- lua/autorun/maintenance_mode.lua
+++ lua/autorun/maintenance_mode.lua
@@ -61,6 +61,16 @@ end
 
 CreateConVar( "in_maintenance", 1, {"FCVAR_ARCHIVE"}, "Toggle maintenance on and off" )
 
+print("Maintenance Mode starting up...\n")
+DaVar = GetConVar( "in_maintenance" )
+if DaVar:GetInt() == 1 then
+  RunConsoleCommand("hostname", "Arty's", "DarkRP", "Dev", "\|/", "Closed", "to", "unranked", "players")
+  print("Server was last in maintenance mode, therefore it will be when server is up.\n")
+else
+  RunConsoleCommand("hostname", "Arty's", "DarkRP", "Dev", "\|/", "Open", "to", "unranked", "players")
+  print("Server was not in maintenance mode, therefore it won't be when server is up.\n")
+end
+
 cvars.AddChangeCallback( "maintenance", function( convar_name, value_old, value_new )
   if convar_name == "in_maintenance" then
     if value_new == 1 then
