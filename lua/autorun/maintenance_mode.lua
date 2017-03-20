eafae21e8ef218d6d29c522791960f64d4c14f49
zacklogan <bigred9921@mchsi.com>
2017-03-20 13:33:36 -0500

    Update maintenance_mode.lua

--- lua/autorun/maintenance_mode.lua
+++ lua/autorun/maintenance_mode.lua
@@ -1,24 +1,28 @@
 -- Main Server --
+
+
 if CLIENT then
-  net.Receive( "PrintColor", function()
-	  chat.AddText( unpack( net.ReadTable() ) )
-  end )
+
+net.Receive( "PrintColor", function()
+  chat.AddText( unpack( net.ReadTable() ) )
+end )
+
 else
 	
-  util.AddNetworkString( "PrintColor" )
+util.AddNetworkString( "PrintColor" )
 
-  local Meta = FindMetaTable( "Player" )
+local Meta = FindMetaTable( "Player" )
 
-  function Meta:PrintColor( ... )
-	  net.Start( "PrintColor" )
-		net.WriteTable( { ... } )
-	  net.Send( self )
-  end
+function Meta:PrintColor( ... )
+  net.Start( "PrintColor" )
+  net.WriteTable( { ... } )
+  net.Send( self )
+end
 	
 CreateConVar( "in_maintenance", 0, {"FCVAR_ARCHIVE"}, "Toggle maintenance on and off" )
 
 hook.Add( "CheckPassword", "access_whitelist", function( steamID64, ip, svpass, clpass, name )
-	Var = GetConVar( "in_maintenance" )
+  Var = GetConVar( "in_maintenance" )
   if Var:GetInt() == 1 then
     p = player.GetBySteamID64( steamID64 )
     if not table.HasValue({"admin","founder","superadmin","mod","dev"}, p:GetNWString("usergroup")) then
@@ -26,31 +30,38 @@ hook.Add( "CheckPassword", "access_whitelist", function( steamID64, ip, svpass, 
         v:PrintColor( Color( 255, 0, 0 ), "Failed Connection ", Color( 0, 0, 0 ), "\|/ ", Color( 255, 255, 255 ), name, " has attempted to connect but was denied.\nReason: Insignificant permissions" )
       end
       return false, "--------[ Arty's DarkRP ]--------\n\n      Undergoing Maintenance"
-		end
-	end
+    end
+  end
 end )
+
 end
+
+
 -- Dev Server --
+
+
 if CLIENT then
-  net.Receive( "PrintColor", function()
-	  chat.AddText( unpack( net.ReadTable() ) )
-  end )
+
+net.Receive( "PrintColor", function()
+  chat.AddText( unpack( net.ReadTable() ) )
+end )
+
 else
 	
-  util.AddNetworkString( "PrintColor" )
+util.AddNetworkString( "PrintColor" )
 
-  local Meta = FindMetaTable( "Player" )
+local Meta = FindMetaTable( "Player" )
 
-  function Meta:PrintColor( ... )
-	  net.Start( "PrintColor" )
-		net.WriteTable( { ... } )
-	  net.Send( self )
-  end
-	
+function Meta:PrintColor( ... )
+  net.Start( "PrintColor" )
+  net.WriteTable( { ... } )
+  net.Send( self )
+end
+
 CreateConVar( "in_maintenance", 1, {"FCVAR_ARCHIVE"}, "Toggle maintenance on and off" )
 
 hook.Add( "CheckPassword", "access_whitelist", function( steamID64, ip, svpass, clpass, name )
-	Var = GetConVar( "in_maintenance" )
+  Var = GetConVar( "in_maintenance" )
   if Var:GetInt() == 1 then
     p = player.GetBySteamID64( steamID64 )
     if not table.HasValue({"admin","founder","superadmin","mod","dev"}, p:GetNWString("usergroup")) then
@@ -58,7 +69,8 @@ hook.Add( "CheckPassword", "access_whitelist", function( steamID64, ip, svpass, 
         v:PrintColor( Color( 255, 0, 0 ), "Failed Connection ", Color( 0, 0, 0 ), "\|/ ", Color( 255, 255, 255 ), name, " has attempted to connect but was denied.\nReason: Insignificant permissions" )
       end
       return false, "---------[ Arty's DarkRP ]---------\n\n           Access Denied\n Reason:  Insignificant permissions"
-		end
-	end
+    end
+  end
 end )
+
 end
