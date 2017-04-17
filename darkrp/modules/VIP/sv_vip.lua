404b02b955d6b156790fea2521ae5b615f30fbb7
zacklogan <bigred9921@mchsi.com>
2017-04-17 10:13:36 -0500

    Update and rename vip.lua to sv_vip.lua

--- darkrp/modules/VIP/sv_vip.lua
+++ darkrp/modules/VIP/sv_vip.lua
@@ -1,6 +1,6 @@
 local function getVIP(ply)
-  if ply:canAfford(100000) then
-    ply:addMoney(-100000)
+  if ply:canAfford(1) then
+    ply:addMoney(-1)
     RunConsoleCommand("sg", "rank " .. name .. " vip 0")
   else
     ply:PrintColor( Color( 255, 0, 0 ), "VIP ", Color( 0, 0, 0 ), "| ", Color( 255, 255, 255 ), "You don't have the required cash! (You need $100,000 in total)")
