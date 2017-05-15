6404a8d75a0ca2d37115566f0fd81e8a94d445f0
zacklogan <bigred9921@mchsi.com>
2017-05-15 10:13:53 -0500

    Create zambiesystem.lua

---
+++ lua/autorun/zambiesystem.lua
@@ -0,0 +1,10 @@
+function fullyloadedthing(ply)
+  -- Connection message blah blah
+  if new then
+    ply:PrintColor( Color( 0, 255, 255 ), "Server | ", Color( 255, 255, 255 ), "Welcome to Buck Pony Servers, ", ply:Nick(), "! Type !spawn or press F2 to get into the action!" )
+    (print all)
+      v:PrintColor( Color( 0, 255, 255 ), "Server | ", Color( 255, 255, 255 ), "Welcome our brand new player, ", ply:Nick(), ", to the server! (#", totaluniqueplayers, ")" )
+  else
+    ply:PrintColor( Color( 0, 255, 255 ), "Server | ", Color( 255, 255, 255 ), "Welcome back to Buck Pony Servers, ", ply:Nick(), "! Type !spawn or press F2 to get into the action!" )
+  end
+end
