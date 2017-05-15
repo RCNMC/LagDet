1cc52db21206ffd1d6cada93d60cc1b44cf8f070
zacklogan <bigred9921@mchsi.com>
2017-05-15 12:59:13 -0500

    Update zambiesystem.lua

--- lua/autorun/zambiesystem.lua
+++ lua/autorun/zambiesystem.lua
@@ -11,15 +11,29 @@ end
 
 hook.Add( "OnPlayerChat", "SpawnCommand", function( ply, strText, bTeam, bDead )
 
-	strText = string.lower( strText ) -- make the string lower case
+	strText = string.lower( strText )
 
-	if ( strText == "!spawn" ) then -- if the player typed /hello then
+	if ( strText == "!spawn" ) then
 		if ply:GetPData( "posx", 0 ) != 0 then
-        -- teleport to location
-        -- give all weapons back and perks
-        ply:PrintColor( Color( 0, 255, 255 ), "Server | ", Color( 255, 255, 255 ), "You've now spawned in and have 30 seconds of spawn protection.  Good luck." )
-        -- start spawn protect and end after 30 seconds
-		return true -- this suppresses the message from being shown
-	end
-
+      -- teleport to location
+      -- give all weapons back and perks
+      ply:PrintColor( Color( 0, 255, 255 ), "Server | ", Color( 255, 255, 255 ), "You've now spawned in and have 30 seconds of spawn protection.  Good luck." )
+      -- start spawn protect and end after 30 seconds
+      ply.Spawned = true
+      return true 
+    end
+	else if ( strText == "!admin" ) then
+	  if ply:GetPData( "staff", 0 ) >= 1 then
+      if ply.Admin == 0 then
+        if ply.Spawned then
+          -- save players data then toggle admin mode
+        else
+          -- Just toggle admin mode
+        end
+      else
+        -- teleport to spawn 
+      end
+    end
+    return true
+  end
 end )
