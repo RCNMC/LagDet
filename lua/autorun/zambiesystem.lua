955ad2eea06ee2ca32adadc21419b29d9cf69d39
zacklogan <bigred9921@mchsi.com>
2017-05-15 12:49:11 -0500

    Update zambiesystem.lua

--- lua/autorun/zambiesystem.lua
+++ lua/autorun/zambiesystem.lua
@@ -8,3 +8,18 @@ function fullyloadedthing(ply)
     ply:PrintColor( Color( 0, 255, 255 ), "Server | ", Color( 255, 255, 255 ), "Welcome back to Buck Pony Servers, ", ply:Nick(), "! Type !spawn or press F2 to get into the action!" )
   end
 end
+
+hook.Add( "OnPlayerChat", "SpawnCommand", function( ply, strText, bTeam, bDead )
+
+	strText = string.lower( strText ) -- make the string lower case
+
+	if ( strText == "!spawn" ) then -- if the player typed /hello then
+		if ply:GetPData( "posx", 0 ) != 0 then
+        -- teleport to location
+        -- give all weapons back and perks
+        ply:PrintColor( Color( 0, 255, 255 ), "Server | ", Color( 255, 255, 255 ), "You've now spawned in and have 30 seconds of spawn protection.  Good luck." )
+        -- start spawn protect and end after 30 seconds
+		return true -- this suppresses the message from being shown
+	end
+
+end )
