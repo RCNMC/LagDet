f115fcd19d35bd01ef3d63fab518ae4dc335c997
zacklogan <bigred9921@mchsi.com>
2017-05-15 09:55:22 -0500

    Create shared.lua

---
+++ lua/entities/jugperk/shared.lua
@@ -0,0 +1,8 @@
+ENT.Type = "anim"
+ENT.Base = "base_gmodentity"
+ 
+ENT.PrintName		= "Jug Perk"
+ENT.Author			= "Logan"
+ENT.Contact			= "via Buck Pony Server's Forums"
+ENT.Purpose			= "PERKS BRUH"
+ENT.Instructions	= "Use it wisely... You're gonna need it."
