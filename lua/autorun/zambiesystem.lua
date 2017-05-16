48ce9dc1373102ed38ce42cc18df3933c7249443
zacklogan <bigred9921@mchsi.com>
2017-05-16 08:17:12 -0500

    Update zambiesystem.lua

--- lua/autorun/zambiesystem.lua
+++ lua/autorun/zambiesystem.lua
@@ -2,8 +2,9 @@ function fullyloadedthing(ply)
   -- Connection message blah blah
   if new then
     ply:PrintColor( Color( 0, 255, 255 ), "Server | ", Color( 255, 255, 255 ), "Welcome to Buck Pony Servers, ", ply:Nick(), "! Type !spawn or press F2 to get into the action!" )
-    (print all)
+    for k, v in pairs( player.GetAll() ) do
       v:PrintColor( Color( 0, 255, 255 ), "Server | ", Color( 255, 255, 255 ), "Welcome our brand new player, ", ply:Nick(), ", to the server! (#", totaluniqueplayers, ")" )
+    end
   else
     ply:PrintColor( Color( 0, 255, 255 ), "Server | ", Color( 255, 255, 255 ), "Welcome back to Buck Pony Servers, ", ply:Nick(), "! Type !spawn or press F2 to get into the action!" )
   end
