747ff7a9953192b3e6e2328b5724ed55c4e6153c
zacklogan <bigred9921@mchsi.com>
2017-05-28 22:25:55 -0700

    Update init.lua

--- lua/entities/jugperk/init.lua
+++ lua/entities/jugperk/init.lua
@@ -1,5 +1,5 @@
-AddCSLuaFile( "cl_init.lua" ) -- Make sure clientside
-AddCSLuaFile( "shared.lua" )  -- and shared scripts are sent.
+AddCSLuaFile( "cl_init.lua" ) 
+AddCSLuaFile( "shared.lua" )  
  
 include('shared.lua')
  
@@ -19,7 +19,7 @@ end
 function ENT:Use( activator, caller )
   if IsValid( caller ) and caller:IsPlayer() then
     if caller:GetPData( "hasJug", 0 ) == 0 then
-		  caller:PrintColor( Color( 0, 255, 0 ), "Perks | ", Color( 255, 255, 255 ), "You've just bought the juggernaut perk! This perk is temporary as it will be removed on death." )
+      caller:PrintColor( Color( 0, 255, 0 ), "Perks | ", Color( 255, 255, 255 ), "You've just bought the juggernaut perk! This perk is temporary as it will be removed on death." )
       caller:SetMaxHealth( 150 )
       caller:SetHealth( 150 )
       caller:SetPData( "hasJug", 1 )
@@ -30,5 +30,5 @@ function ENT:Use( activator, caller )
 end
  
 function ENT:Think()
-    -- We don't need to think, we are just a prop after all!
+
 end
