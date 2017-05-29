15a6b031d405e447210d6fe7b2d388b0a51cee3b
zacklogan <bigred9921@mchsi.com>
2017-05-28 22:25:20 -0700

    Update cl_init.lua

--- lua/entities/jugperk/cl_init.lua
+++ lua/entities/jugperk/cl_init.lua
@@ -1,7 +1,5 @@
 include('shared.lua')
 
 function ENT:Draw()
-    -- self.BaseClass.Draw(self)  -- We want to override rendering, so don't call baseclass.
-                                  -- Use this when you need to add to the rendering.
-    self:DrawModel()       -- Draw the model.
+    self:DrawModel()       
 end
